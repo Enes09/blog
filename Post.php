c4a5c01a18f136165b7520decedd01d3209a20a0
Enes09 <enes.er2709@gmail.com>
2018-09-13 16:02:49 +0200

    Nous avons développer notre premier model qui est Post puis avons développer notre permière class contenant permetant pour l'instant de créer un post dans la base de donée en faisant une instance puis en utilisant la method create

---
+++ Post.php
@@ -0,0 +1,46 @@
+<?php 
+class Post {
+
+	public $_title;
+	public $_content;
+
+	public function __construct($title,$content){
+
+		$this->setTitle($title);
+		$this->setContent($content);
+	}
+
+	public function setTitle($title){
+
+		if (strlen($title) < 255 )
+			{
+				$this->_title = $title;
+			}
+		else
+			{
+				throw new Execpetion ("Le titre de votre billet doit être plus court.");
+			}
+	}
+
+	public function setContent($content){
+
+		$this->_content = $content;
+	}
+
+	public function dbConnect (){
+
+		$db = new PDO ('mysql:host=localhost;dbname=blog','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		return $db;
+	} 
+
+	public function Create(){
+
+		$db = $this->dbConnect();
+		$prepare = $db->prepare('INSERT INTO posts (title,content,post_date) VALUES (?, ?, NOW())');
+		$post = $prepare->execute(array($this->_title, $this->_content));
+
+		return $post;
+
+	}
+}
+
